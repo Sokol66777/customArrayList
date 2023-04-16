b70435653afa6bc80ee0ff9232f2f8f5abf3b644
Sokol66777 <Sokol66777@gmail.com>
2023-04-16 21:16:48 +0300

    fix mistake

--- src/main/test/java/CustomArrayList/CustomArrayTest.java
+++ src/main/test/java/CustomArrayList/CustomArrayTest.java
@@ -1,16 +1,28 @@
 package CustomArrayList;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CustomArrayTest {
 
-    @Test
-    public void testDeleteElement(){
-        Array<String> strs = new CustomArray<>();
+    Array<String> strs = new CustomArray<>();
+    @BeforeEach
+    public void initializationTestList(){
         strs.add("str1");
         strs.add("str2");
         strs.add("str3");
+    }
+
+    @AfterEach
+    public void cleanList(){
+        strs=null;
+    }
+
+    @Test
+    public void testDeleteElement(){
+
         strs.delete(1);
 
         assertEquals(2,strs.size());
@@ -18,18 +30,13 @@ public class CustomArrayTest {
 
     @Test
     public void testAddElement(){
-        Array<String> strs = new CustomArray<>();
-        strs.add("str2");
-
         String result = "str2";
 
-        assertEquals(result,strs.get(0));
+        assertEquals(result,strs.get(1));
     }
 
     @Test
     public void testUpdateElement(){
-        Array<String> strs = new CustomArray<>();
-        strs.add("str2");
         strs.update(0,"update");
 
         String result = "update";
@@ -39,11 +46,6 @@ public class CustomArrayTest {
 
     @Test
     public void testSize(){
-        Array<String> strs = new CustomArray<>();
-        strs.add("str1");
-        strs.add("str2");
-        strs.add("str3");
-
         int result = 3;
 
         assertEquals(result,strs.size());
